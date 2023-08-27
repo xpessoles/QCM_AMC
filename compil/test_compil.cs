ef9f38c9b409384fe90d7bb1050c6b91c49491b9
Xavier Pessoles <xpessoles.ptsi@free.fr>
2023-08-27 20:30:14 +0200

    MAJ

---
+++ compil/test_compil.cs
@@ -0,0 +1,52 @@
+\answer{0/1:case:OpEl01:1,1}{}
+\answer{0/1:case:OpEl01:1,2}{}
+\answer{0/1:case:OpEl01:1,3}{}
+\answer{0/1:case:OpEl01:1,4}{}
+\answer{0/1:case:OpEl02:2,1}{}
+\answer{0/1:case:OpEl02:2,2}{}
+\answer{0/1:case:OpEl02:2,3}{}
+\answer{0/1:case:OpEl02:2,4}{}
+\answer{0/1:case::3,1}{}
+\answer{0/1:case::3,2}{}
+\answer{0/1:case::3,3}{}
+\answer{0/1:case::3,4}{}
+\answer{0/1:case:OpAr01:4,1}{}
+\answer{0/1:case:OpAr01:4,2}{}
+\answer{0/1:case:OpAr01:4,3}{}
+\answer{0/1:case:OpAr01:4,4}{}
+\answer{0/1:case:entiers 01:5,1}{}
+\answer{0/1:case:entiers 01:5,2}{}
+\answer{0/1:case:entiers 01:5,3}{}
+\answer{0/1:case:entiers 01:5,4}{}
+\answer{0/1:case:entiers 02:6,1}{}
+\answer{0/1:case:entiers 02:6,2}{}
+\answer{0/1:case:entiers 02:6,3}{}
+\answer{0/1:case:entiers 02:6,4}{}
+\answer{0/1:case:entiers 03:7,1}{}
+\answer{0/1:case:entiers 03:7,2}{}
+\answer{0/1:case:entiers 03:7,3}{}
+\answer{0/1:case:entiers 03:7,4}{}
+\answer{0/1:case:relatifs 01:8,1}{}
+\answer{0/1:case:relatifs 01:8,2}{}
+\answer{0/1:case:relatifs 01:8,3}{}
+\answer{0/1:case:relatifs 01:8,4}{}
+\answer{0/1:case:hexa 01:9,1}{}
+\answer{0/1:case:hexa 01:9,2}{}
+\answer{0/1:case:hexa 01:9,3}{}
+\answer{0/1:case:hexa 01:9,4}{}
+\answer{0/1:case:hexa 01:9,1}{}
+\answer{0/1:case:hexa 01:9,2}{}
+\answer{0/1:case:hexa 01:9,3}{}
+\answer{0/1:case:hexa 01:9,4}{}
+\answer{0/1:case:boole 01:10,1}{}
+\answer{0/1:case:boole 01:10,2}{}
+\answer{0/1:case:boole 01:10,3}{}
+\answer{0/1:case:boole 01:10,4}{}
+\answer{0/2:case:str01:11,1}{}
+\answer{0/2:case:str01:11,2}{}
+\answer{0/2:case:str01:11,3}{}
+\answer{0/2:case:str01:11,4}{}
+\answer{0/2:case:str:12,1}{}
+\answer{0/2:case:str:12,2}{}
+\answer{0/2:case:str:12,3}{}
+\answer{0/2:case:str:12,4}{}
