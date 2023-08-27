c843f482c76094cd918eae62d229308f8153f3c0
Xavier Pessoles <xpessoles.ptsi@free.fr>
2023-08-27 22:07:21 +0200

    MAJ

--- compil/test_compil.cs
+++ compil/test_compil.cs
@@ -6,47 +6,79 @@
 \answer{0/1:case:OpEl02:2,2}{}
 \answer{0/1:case:OpEl02:2,3}{}
 \answer{0/1:case:OpEl02:2,4}{}
-\answer{0/1:case::3,1}{}
-\answer{0/1:case::3,2}{}
-\answer{0/1:case::3,3}{}
-\answer{0/1:case::3,4}{}
-\answer{0/1:case:OpAr01:4,1}{}
-\answer{0/1:case:OpAr01:4,2}{}
-\answer{0/1:case:OpAr01:4,3}{}
-\answer{0/1:case:OpAr01:4,4}{}
-\answer{0/1:case:entiers 01:5,1}{}
-\answer{0/1:case:entiers 01:5,2}{}
-\answer{0/1:case:entiers 01:5,3}{}
-\answer{0/1:case:entiers 01:5,4}{}
-\answer{0/1:case:entiers 02:6,1}{}
-\answer{0/1:case:entiers 02:6,2}{}
-\answer{0/1:case:entiers 02:6,3}{}
-\answer{0/1:case:entiers 02:6,4}{}
-\answer{0/1:case:entiers 03:7,1}{}
-\answer{0/1:case:entiers 03:7,2}{}
-\answer{0/1:case:entiers 03:7,3}{}
-\answer{0/1:case:entiers 03:7,4}{}
-\answer{0/1:case:relatifs 01:8,1}{}
-\answer{0/1:case:relatifs 01:8,2}{}
-\answer{0/1:case:relatifs 01:8,3}{}
-\answer{0/1:case:relatifs 01:8,4}{}
-\answer{0/1:case:hexa 01:9,1}{}
-\answer{0/1:case:hexa 01:9,2}{}
-\answer{0/1:case:hexa 01:9,3}{}
-\answer{0/1:case:hexa 01:9,4}{}
-\answer{0/1:case:hexa 01:9,1}{}
-\answer{0/1:case:hexa 01:9,2}{}
-\answer{0/1:case:hexa 01:9,3}{}
-\answer{0/1:case:hexa 01:9,4}{}
-\answer{0/1:case:boole 01:10,1}{}
-\answer{0/1:case:boole 01:10,2}{}
-\answer{0/1:case:boole 01:10,3}{}
-\answer{0/1:case:boole 01:10,4}{}
-\answer{0/2:case:str01:11,1}{}
-\answer{0/2:case:str01:11,2}{}
-\answer{0/2:case:str01:11,3}{}
-\answer{0/2:case:str01:11,4}{}
-\answer{0/2:case:str:12,1}{}
-\answer{0/2:case:str:12,2}{}
-\answer{0/2:case:str:12,3}{}
-\answer{0/2:case:str:12,4}{}
+\answer{0/1:case:OpEl03:3,1}{}
+\answer{0/1:case:OpEl03:3,2}{}
+\answer{0/1:case:OpEl03:3,3}{}
+\answer{0/1:case:OpEl03:3,4}{}
+\answer{0/1:case:OpEl:4,1}{}
+\answer{0/1:case:OpEl:4,2}{}
+\answer{0/1:case:OpEl:4,3}{}
+\answer{0/1:case:OpEl:4,4}{}
+\answer{0/1:case:OpAr01:5,1}{}
+\answer{0/1:case:OpAr01:5,2}{}
+\answer{0/1:case:OpAr01:5,3}{}
+\answer{0/1:case:OpAr01:5,4}{}
+\answer{0/1:case:entiers 01:6,1}{}
+\answer{0/1:case:entiers 01:6,2}{}
+\answer{0/1:case:entiers 01:6,3}{}
+\answer{0/1:case:entiers 01:6,4}{}
+\answer{0/1:case:entiers 02:7,1}{}
+\answer{0/1:case:entiers 02:7,2}{}
+\answer{0/1:case:entiers 02:7,3}{}
+\answer{0/1:case:entiers 02:7,4}{}
+\answer{0/1:case:entiers 03:8,1}{}
+\answer{0/1:case:entiers 03:8,2}{}
+\answer{0/1:case:entiers 03:8,3}{}
+\answer{0/1:case:entiers 03:8,4}{}
+\answer{0/1:case:relatifs 01:9,1}{}
+\answer{0/1:case:relatifs 01:9,2}{}
+\answer{0/1:case:relatifs 01:9,3}{}
+\answer{0/1:case:relatifs 01:9,4}{}
+\answer{0/2:case:hexa 01:10,1}{}
+\answer{0/2:case:hexa 01:10,2}{}
+\answer{0/2:case:hexa 01:10,3}{}
+\answer{0/2:case:hexa 01:10,4}{}
+\answer{0/2:case:hexa 01:10,1}{}
+\answer{0/2:case:hexa 01:10,2}{}
+\answer{0/2:case:hexa 01:10,3}{}
+\answer{0/2:case:hexa 01:10,4}{}
+\answer{0/2:case:boole 01:11,1}{}
+\answer{0/2:case:boole 01:11,2}{}
+\answer{0/2:case:boole 01:11,3}{}
+\answer{0/2:case:boole 01:11,4}{}
+\answer{0/2:case:str01:12,1}{}
+\answer{0/2:case:str01:12,2}{}
+\answer{0/2:case:str01:12,3}{}
+\answer{0/2:case:str01:12,4}{}
+\answer{0/2:case:str:13,1}{}
+\answer{0/2:case:str:13,2}{}
+\answer{0/2:case:str:13,3}{}
+\answer{0/2:case:str:13,4}{}
+\answer{0/2:case::14,1}{}
+\answer{0/2:case::14,2}{}
+\answer{0/2:case::14,3}{}
+\answer{0/2:case::14,4}{}
+\answer{0/2:case:TtQue01:15,1}{}
+\answer{0/2:case:TtQue01:15,2}{}
+\answer{0/2:case:TtQue01:15,3}{}
+\answer{0/2:case:TtQue01:15,4}{}
+\answer{0/2:case::14,1}{}
+\answer{0/2:case::14,2}{}
+\answer{0/2:case::14,3}{}
+\answer{0/2:case::14,4}{}
+\answer{0/2:case:for01:16,1}{}
+\answer{0/2:case:for01:16,2}{}
+\answer{0/2:case:for01:16,3}{}
+\answer{0/2:case:for01:16,4}{}
+\answer{0/2:case:for02:17,1}{}
+\answer{0/2:case:for02:17,2}{}
+\answer{0/2:case:for02:17,3}{}
+\answer{0/2:case:for02:17,4}{}
+\answer{0/2:case:for:18,1}{}
+\answer{0/2:case:for:18,2}{}
+\answer{0/2:case:for:18,3}{}
+\answer{0/2:case:for:18,4}{}
+\answer{0/3:case:fonclist01:19,1}{}
+\answer{0/3:case:fonclist01:19,2}{}
+\answer{0/3:case:fonclist01:19,3}{}
+\answer{0/3:case:fonclist01:19,4}{}
